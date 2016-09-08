447c58216bfb1d4c74bad808e6c111cf571613cc
david Eduardo Ortiz Velasco <davideduardo.ortiz@gmail.com>
2016-09-07 19:24:14 -0500

    Se agregaron media querys para news container

--- src/components/NewsContainer.js
+++ src/components/NewsContainer.js
@@ -8,10 +8,10 @@ class NewsContainer extends React.Component {
   render() {
     return (
       <div id="panel-news" className="row">
-      <div className="col-md-2">
+      <div className="col-xs-4 col-sm-2 col-md-2">
       <input className="img-circle" type="image" src={yeo} alt="yeoman" />
       </div>
-      <div id="cajita" className="col-md-8">
+      <div id="cajita" className="col-xs-8 col-sm-9 col-md-8">
       <button type="button" className="news-button">
       Falcao pide no ir sentado en el bus de la selección
       </button>
