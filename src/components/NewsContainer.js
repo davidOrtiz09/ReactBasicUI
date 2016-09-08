f2163f75a13ced8709c3a74144bb540ef9bbb33c
david Eduardo Ortiz Velasco <davideduardo.ortiz@gmail.com>
2016-09-07 23:36:58 -0500

    Se agrego responsive a los paneles con las noticias

--- src/components/NewsContainer.js
+++ src/components/NewsContainer.js
@@ -1,5 +1,8 @@
 
 var React = require('react');
+var sadFalco = 'http://www.las2orillas.co/wp-content/uploads/2014/01/falcao_garcia3.jpg'
+var falca = 'Una novedad inesperada trajo el nuevo bus de la selección Colombia que será estrenado por el equipo de Pekerman la próxima semana cuando este se reúna para enfrentar a Perú en Barranquilla por la primera fecha de las eliminatorias a Rusia 2018.'
+var Detail =  require('./NewsDetail');
 require('normalize.css/normalize.css');
 require('styles/newsContainers.css');
 
@@ -17,6 +20,7 @@ componentDidMount:function() {
 },
   render:function() {
     return (
+      <div>
       <div id='panel-news' className='row'>
       <div className='col-xs-4 col-sm-2 col-md-2'>
       <input className='img-circle' type='image' src={this.state.iconColor} alt='arrow icon' />
@@ -27,6 +31,8 @@ componentDidMount:function() {
       </button>
       </div>
     </div>
+    <Detail image={sadFalco} show={true} paragraph={falca}/>
+  </div>
     );
   }
 });
--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -1,14 +1,29 @@
 
 var React = require('react');
 require('normalize.css/normalize.css');
+require('styles/detailsContainer.css');
+
 
 var  NewsDetail = React.createClass({
 
   render:function() {
+  if(this.props.show){
     return (
-
+      <div id='panel-details' className='row'>
+      <div className='col-xs-4 col-sm-2 col-md-3' id='img-container'>
+      <img src={this.props.image} alt="Smiley falcao" id='img-detail' />
+      </div>
+      <div className='col-xs-8 col-sm-9 col-md-8'>
+      <p className='para-news'>
+        {this.props.paragraph}
+      </p>
+      </div>
+    </div>
     );
+  } else{
+    return(<div></div>)
   }
+}
 });
 
 module.exports = NewsDetail;
